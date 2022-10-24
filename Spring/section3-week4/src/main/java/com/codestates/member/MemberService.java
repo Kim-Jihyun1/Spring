6b1a9c9b26601aea56339f25790b9867f1ffcf39
Kim-Jihyun1 <jh129047@gmail.com>
2022-10-24 22:11:41 +0900

    @Service 애너테이션 추가 -> Spring Bean (V3)

--- Spring/section3-week4/src/main/java/com/codestates/member/MemberService.java
+++ Spring/section3-week4/src/main/java/com/codestates/member/MemberService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service // Spring Bean
+@Service
 public class MemberService {
     public Member createMember(Member member) {
         // member 객체는 나중에 DB에 저장 이후 되돌려 받는 것으로 변경
