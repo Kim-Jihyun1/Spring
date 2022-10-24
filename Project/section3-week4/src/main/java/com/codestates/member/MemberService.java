cb079879ea41bcc6ab92bd0f9ed761ed9dd294ce
Kim-Jihyun1 <jh129047@gmail.com>
2022-10-24 21:36:20 +0900

    서비스 계층에서의 DI 실습 (v1)

---
+++ Project/section3-week4/src/main/java/com/codestates/member/MemberService.java
@@ -0,0 +1,37 @@
+package com.codestates.member;
+
+import java.lang.reflect.Member;
+import java.util.List;
+
+public class MemberService {
+    public Member createMember(Member member) {
+        // member 객체는 나중에 DB에 저장 이후 되돌려 받는 것으로 변경
+        Member createdMember = member;
+        return createdMember;
+    }
+
+    public Member updateMember(Member member) {
+        // member 객체는 DB에 업데이트 이후 되돌려 받는 것으로 변경
+        Member updatedMember = member;
+        return updatedMember;
+    }
+
+    public Member findMember(long memberId) {
+        // member 객체는 나중에 DB에서 조회
+        Member member =
+                new Member(memberId, "hgd@gmail.com", "홍길동", "010-1234-5678");
+        return member;
+    }
+
+    public List<Member> findMembers() {
+        // member 객체는 나중에 DB에서 조회
+        List<Member> members = List.of(
+                new Member(1, "hgd@gmail.com", "홍길동", "010-1234-5678"),
+                new Member(2, "lml@gmail.com", "이몽룡", "010-1111-2222")
+        );
+        return members;
+    }
+
+    public void deleteMember(long memberId) {
+    }
+}
