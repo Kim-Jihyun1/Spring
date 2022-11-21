7809ce275ceed7313ac5d7ba2412c202823afebd
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-21 16:23:47 +0900

    응답 데이터에 ResponseEntity 적용

--- Project/section3-review/src/main/java/com/codestates/member/MemberController.java
+++ Project/section3-review/src/main/java/com/codestates/member/MemberController.java
@@ -1,41 +1,53 @@
 package com.codestates.member;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE}) // JSON 형식의 데이터를 응답 데이터로 전송
+@RequestMapping("/v1/members") // produces 설정 제거 (value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class MemberController {
     // 회원 정보를 등록하는 핸들러 메서드
     @PostMapping
-    public String postMember(@RequestParam("email") String email, @RequestParam("name") String name, @RequestParam("phone") String phone) {
+    public ResponseEntity postMember(@RequestParam("email") String email, @RequestParam("name") String name, @RequestParam("phone") String phone) {
         System.out.println("# email: " + email);
         System.out.println("# name: " + name);
         System.out.println("# phone: " + phone);
 
-        String response =
-                "{\"" +
-                        "email\":\"" + email + "\"," +
-                        "\"name\":\"" + name + "\",\"" +
-                        "phone\":\"" + phone + "\"}";
-        return response;
+//        String response =
+//                "{\"" +
+//                        "email\":\"" + email + "\"," +
+//                        "\"name\":\"" + name + "\",\"" +
+//                        "phone\":\"" + phone + "\"}";
+//        return response;
+        // JSON 문자열을 수작업으로 작성한 부분을 Map 객체로 대체
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
+
+        // 리턴 값을 ResponseEntity 객체로 변경
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     // 특정 회원 정보 조회
     @GetMapping("/{member-id}")
-    public String getMember(@PathVariable("member-id") long memberId) {
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId: " + memberId);
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK); // 리턴 값을 ResponseEntity 객체로 변경
     }
 
     // 전체 회원 목록 조회
     @GetMapping
-    public String getMembers() {
+    public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK); // 리턴 값을 ResponseEntity 객체로 변경
     }
 }
