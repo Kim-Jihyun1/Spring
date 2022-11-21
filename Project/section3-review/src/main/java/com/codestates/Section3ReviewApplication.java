ed3720257b3e8b68ae21252d4c2f3bfafd7f8a06
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-21 13:41:24 +0900

    section3 exercise

---
+++ Project/section3-review/src/main/java/com/codestates/Section3ReviewApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3ReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3ReviewApplication.class, args);
+	}
+
+}
