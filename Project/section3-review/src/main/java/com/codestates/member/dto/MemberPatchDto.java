1505a4a71ede143a9ec6525f48dc8017e76fc729
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-22 20:25:01 +0900

    Custom Validator를 사용한 유효성 검증 (Custom Annotation)

--- Project/section3-review/src/main/java/com/codestates/member/dto/MemberPatchDto.java
+++ Project/section3-review/src/main/java/com/codestates/member/dto/MemberPatchDto.java
@@ -1,11 +1,13 @@
 package com.codestates.member.dto;
 
+import com.codestates.validator.NotSpace;
+
 import javax.validation.constraints.Pattern;
 
 public class MemberPatchDto {
     private long memberId;
 
-    @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "회원 이름은 공백이 아니어야 합니다.")
+    @NotSpace(message = "회원 이름은 공백이 아니어야 합니다.") // @Pattern 애너테이션 삭제
     private String name;
 
     @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$", message = "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되어야 합니다.")
