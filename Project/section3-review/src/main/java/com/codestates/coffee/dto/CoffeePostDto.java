14466dab59a5a3dfd58a5bbcee5e9d63e6df6f94
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-22 15:39:51 +0900

    MemberController에 DTO 적용 + CoffeePatchDto, CoffeePostDto 클래스 구현

---
+++ Project/section3-review/src/main/java/com/codestates/coffee/dto/CoffeePostDto.java
@@ -0,0 +1,24 @@
+package com.codestates.coffee.dto;
+
+public class CoffeePostDto {
+    private String coffeeId;
+    private String korName;
+    private String engName;
+    private int price;
+
+    public String getCoffeeId() {
+        return coffeeId;
+    }
+
+    public String getKorName() {
+        return korName;
+    }
+
+    public String getEngName() {
+        return engName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
--- Project/section3-review/src/main/java/com/codestates/member/MemberController.java
+++ Project/section3-review/src/main/java/com/codestates/member/MemberController.java
@@ -1,5 +1,7 @@
 package com.codestates.member;
 
+import com.codestates.member.dto.MemberPatchDto;
+import com.codestates.member.dto.MemberPostDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,52 +10,39 @@ import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
+// DTO 적용(postMember, patchMember)
 @RestController
 @RequestMapping("/v1/members")
 public class MemberController {
-    private final Map<Long, Map<String, Object>> members = new HashMap<>();
-
-    @PostConstruct
-    public void init() {
-        Map<String, Object> member1 = new HashMap<>();
-        long memberId = 1L;
-        member1.put("memberId", memberId);
-        member1.put("email", "hgd@gmail.com");
-        member1.put("name", "홍길동");
-        member1.put("phone", "010-1234-5678");
-
-        members.put(memberId, member1);
-    }
+//    private final Map<Long, Map<String, Object>> members = new HashMap<>();
+//
+//    @PostConstruct
+//    public void init() {
+//        Map<String, Object> member1 = new HashMap<>();
+//        long memberId = 1L;
+//        member1.put("memberId", memberId);
+//        member1.put("email", "hgd@gmail.com");
+//        member1.put("name", "홍길동");
+//        member1.put("phone", "010-1234-5678");
+//
+//        members.put(memberId, member1);
+//    }
 
     // 회원 정보 등록
     @PostMapping
-    public ResponseEntity postMember(@RequestParam("email") String email, @RequestParam("name") String name, @RequestParam("phone") String phone) {
-        System.out.println("# email: " + email);
-        System.out.println("# name: " + name);
-        System.out.println("# phone: " + phone);
-
-        Map<String, String> map = new HashMap<>();
-        map.put("email", email);
-        map.put("name", name);
-        map.put("phone", phone);
-
-
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) { // @RequestBody : JSON -> DTO (역직렬화), @ResponseBody : DTO -> JSON 직렬화)
+        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
     }
 
     // 회원 정보 수정
-    @PatchMapping
-    public ResponseEntity patchMember(@PathVariable("member-id") long memberId, @RequestParam String phone) { // phone 정보 수정
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") long memberId, @RequestBody MemberPatchDto memberPatchDto) { // DTO 적용
+        memberPatchDto.setMemberId(memberId);
+        memberPatchDto.setName("홍길동");
+
         // No need Business logic
 
-        Map<String, Object> member = members.get(memberId);
-
-        if (member == null)
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        else
-            member.put("phone", phone);
-
-        return new ResponseEntity<>(member, HttpStatus.OK);
+        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
     }
 
     // 특정 회원 정보 조회
@@ -71,16 +60,14 @@ public class MemberController {
         System.out.println("# get Members");
 
         // not implementation
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // 회원 정보 삭제
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
-        if (members.containsKey(memberId))
-            members.remove(memberId);
-        else
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        // No need Business logic
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 회원 정보가 삭제되어 존재하지 않으므로 204 No Content를 지정
     }
