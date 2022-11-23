0603936bbd4a315c080245094d6afcbfdbeb0b1a
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-23 20:21:51 +0900

    DI 없이 비즈니스 계층과 API 계층 연동

--- Project/section3-review/src/main/java/com/codestates/member/controller/MemberController.java
+++ Project/section3-review/src/main/java/com/codestates/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.codestates.member.controller;
 
 import com.codestates.member.dto.MemberPatchDto;
 import com.codestates.member.dto.MemberPostDto;
+import com.codestates.member.entity.Member;
+import com.codestates.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -9,41 +11,46 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/members")
 @Validated // 유효성 검증이 정상적으로 수행되기 위함
 public class MemberController {
-//    private final Map<Long, Map<String, Object>> members = new HashMap<>();
-//
-//    @PostConstruct
-//    public void init() {
-//        Map<String, Object> member1 = new HashMap<>();
-//        long memberId = 1L;
-//        member1.put("memberId", memberId);
-//        member1.put("email", "hgd@gmail.com");
-//        member1.put("name", "홍길동");
-//        member1.put("phone", "010-1234-5678");
-//
-//        members.put(memberId, member1);
-//    }
+    private final MemberService memberService;
+
+    public MemberController() {
+        this.memberService = new MemberService();
+    }
 
     // 회원 정보 등록
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
-        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
+        Member member = new Member();
+        member.setEmail(memberPostDto.getEmail());
+        member.setName(memberPostDto.getName());
+        member.setPhone(memberPostDto.getPhone());
+
+        Member response = memberService.createMember(member); // 서비스 계층과의 연결 지점
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     // 회원 정보 수정
     @PatchMapping("/{member-id}")
-    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId, // @Min(1) : memberId가 1 이상의 숫자
+    public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
                                       @Valid @RequestBody MemberPatchDto memberPatchDto) {
         memberPatchDto.setMemberId(memberId);
-        memberPatchDto.setName("홍길동");
 
-        // No need Business logic
+        Member member = new Member();
+        member.setMemberId(memberPatchDto.getMemberId());
+        member.setName(memberPatchDto.getName());
+        member.setPhone(memberPatchDto.getPhone());
 
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member response = memberService.updateMember(member); // 서비스 계층과의 연결 지점
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 특정 회원 정보 조회
@@ -51,8 +58,9 @@ public class MemberController {
     public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId: " + memberId);
 
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
+        Member response = memberService.findMember(memberId); // 서비스 계층과의 연결 지점
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 전체 회원 목록 조회
@@ -60,17 +68,18 @@ public class MemberController {
     public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
-        // not implementation
+        List<Member> response = memberService.findMembers();
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 회원 정보 삭제
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
-        System.out.println("# memberId: " + memberId);
+        System.out.println("# delete member");
 
-        // No need Business logic
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 회원 정보가 삭제되어 존재하지 않으므로 204 No Content를 지정
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT); // 회원 정보가 삭제되어 존재하지 않으므로 204 No Content를 지정
     }
 }
