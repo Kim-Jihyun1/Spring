66743e7e39c2f90dce850c0065c7794976240640
Kim-Jihyun1 <jh129047@gmail.com>
2022-11-29 21:09:08 +0900

    서비스 클래스 구현

--- Project/section3-review/src/main/java/com/codestates/member/service/MemberService.java
+++ Project/section3-review/src/main/java/com/codestates/member/service/MemberService.java
@@ -3,40 +3,76 @@ package com.codestates.member.service;
 import com.codestates.exception.BusinessLogicException;
 import com.codestates.exception.ExceptionCode;
 import com.codestates.member.entity.Member;
+import com.codestates.member.repository.MemberRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MemberService {
-    public Member createMember(Member member) {
-        // TODO should business logic
+    private final MemberRepository memberRepository;
 
-        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member createMember(Member member) {
+        // 이미 등록된 이메일인지 확인
+        verifyExistsEmail(member.getEmail());
+
+        return memberRepository.save(member);
     }
 
     public Member updateMember(Member member) {
-        // TODO should business logic
+        Member findMember = findVerifiedMember(member.getMemberId());
 
-        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+        Optional.ofNullable(member.getName())
+                .ifPresent(name -> findMember.setName(name));
+        Optional.ofNullable(member.getPhone())
+                .ifPresent(phone -> findMember.setPhone(phone));
+        // 회원의 상태 정보 업데이트
+        Optional.ofNullable(member.getMemberStatus())
+                .ifPresent(memberStatus -> findMember.setMemberStatus(memberStatus));
+
+        // 수정 시간 업데이트
+        findMember.setModifiedAt(LocalDateTime.now());
+
+        return memberRepository.save(findMember);
     }
 
     public Member findMember(long memberId) {
-        // TODO should business logic
-
-        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+        return findVerifiedMember(memberId);
     }
 
     public Page<Member> findMembers(int page, int size) {
-        // TODO should business logic
-
-        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+        return memberRepository.findAll(PageRequest.of(page, size, Sort.by("memberId").descending()));
     }
 
     public void deleteMember(long memberId) {
-        // TODO should business logic
+        Member findMember = findVerifiedMember(memberId);
 
-        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+        memberRepository.delete(findMember);
+    }
+
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return findMember;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+
+        if (member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
     }
 }
--- Project/section3-review/src/main/java/com/codestates/order/service/OrderService.java
+++ Project/section3-review/src/main/java/com/codestates/order/service/OrderService.java
@@ -1,27 +1,76 @@
 package com.codestates.order.service;
 
+import com.codestates.exception.BusinessLogicException;
+import com.codestates.exception.ExceptionCode;
+import com.codestates.member.service.MemberService;
 import com.codestates.order.entity.Order;
+import com.codestates.order.repository.OrderRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
+    private final MemberService memberService;
+    private final OrderRepository orderRepository;
+
+    public OrderService(MemberService memberService, OrderRepository orderRepository) {
+        this.memberService = memberService;
+        this.orderRepository = orderRepository;
+    }
+
     public Order createOrder(Order order) {
-        return order;
+        // 회원이 존재하는지 확인
+        memberService.findVerifiedMember(order.getMember().getMemberId());
+
+        // TODO 커피가 존재하는지 조회
+
+        return orderRepository.save(order);
+    }
+
+    // 주문 상태 처리
+    public Order updateOrder(Order order) {
+        Order findOrder = findVerifiedOrder(order.getOrderId());
+
+        Optional.ofNullable(order.getOrderStatus())
+                .ifPresent(orderStatus -> findOrder.setOrderStatus(orderStatus));
+        findOrder.setModifiedAt(LocalDateTime.now());
+        return orderRepository.save(findOrder);
     }
 
     public Order findOrder(long orderId) {
-        return new Order(1L, 1L);
+        return findVerifiedOrder(orderId);
     }
 
-    // 주문 수정 메서드는 사용하지 않습니다.
-
-    public List<Order> findOrders() {
-        return List.of(new Order(1L, 1L),
-                new Order(2L, 2L));
+    public Page<Order> findOrders(int page, int size) {
+        return orderRepository.findAll(PageRequest.of(page, size, Sort.by("orderId").descending()));
     }
 
-    public void cancelOrder() {
+    public void cancelOrder(long orderId) {
+        Order findOrder = findVerifiedOrder(orderId);
+
+        int step = findOrder.getOrderStatus().getStepNumber();
+
+        // OrderStatus의 step이 2 이상일 경우(ORDER_CONFIRM) 주문 취소 불가
+        if (step >= 2)
+            throw new BusinessLogicException(ExceptionCode.CANNOT_CHANGE_ORDER);
+
+        findOrder.setOrderStatus(Order.OrderStatus.ORDER_CANCEL);
+        findOrder.setModifiedAt(LocalDateTime.now());
+        orderRepository.save(findOrder);
+    }
+
+    private Order findVerifiedOrder(long orderId) {
+        Optional<Order> optionalOrder = orderRepository.findById(orderId);
+        Order findOrder =
+                optionalOrder.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.ORDER_NOT_FOUND));
+
+        return findOrder;
     }
 }
