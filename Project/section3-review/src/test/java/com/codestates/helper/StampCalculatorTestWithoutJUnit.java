27e3d2f93e1afb1913a443a72af0bae237eaa47c
Kim-Jihyun1 <jh129047@gmail.com>
2022-12-06 22:53:27 +0900

    단위 테스트(Unit Test)

---
+++ Project/section3-review/src/test/java/com/codestates/helper/StampCalculatorTestWithoutJUnit.java
@@ -0,0 +1,46 @@
+package com.codestates.helper;
+
+import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
+
+import java.util.List;
+
+public class StampCalculatorTestWithoutJUnit {
+    public static void main(String[] args) {
+        calculateStampCountTest();
+        calculateEarnedStampCountTest();
+    }
+
+    public static void calculateStampCountTest() {
+        // given : 테스트에 필요한 전제 조건
+        int nowCount = 5;
+        int earned = 3;
+
+        // when : 테스트 할 대상(동작)
+        int actual = StampCalculator.calculateStampCount(nowCount, earned);
+        int expected = 7; // 기대값
+
+        // then : 테스트 결과 검증
+        System.out.println(expected == actual); // 계산 결과 = 8 -> false
+    }
+
+    public static void calculateEarnedStampCountTest() {
+        // given
+        Order order = new Order();
+        OrderCoffee orderCoffee1 = new OrderCoffee();
+        orderCoffee1.setQuantity(3);
+
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+        orderCoffee2.setQuantity(5);
+
+        order.setOrderCoffees(List.of(orderCoffee1, orderCoffee2));
+
+        int expected = orderCoffee1.getQuantity() + orderCoffee2.getQuantity();
+
+        // when
+        int actual = StampCalculator.calculateEarnedStampCount(order);
+
+        // then
+        System.out.println(expected == actual);
+    }
+}
